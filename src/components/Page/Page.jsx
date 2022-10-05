a76e179e4e891854b96ba025f5ef41eb0cc025ac
Riat Sakhapov <sakhapov.riat@gmail.com>
2022-10-05 04:00:03 +0300

    some fixes

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css"
 export const Page = () => {
     return (
         <div>
-            <div className={styles.container}>
+            <div className={styles.container} onMouseDown={() => {return false}}>
                 <h1 className={styles.header}>Рассчитатйте стоимость автомобиля в лизинг</h1>
                 <Form></Form>
             </div>
